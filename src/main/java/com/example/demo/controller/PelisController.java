d81a718172d6cfc4d1f71c65172b8938d8b0773c
Juan Patino <jjpg2001@hotmail.com>
2021-11-02 16:17:12 -0500

    segundo commit

--- src/main/java/com/example/demo/controller/PelisController.java
+++ src/main/java/com/example/demo/controller/PelisController.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.model.InformacionI;
-import com.example.demo.model.InformacionImpl;
 import com.example.demo.model.Pelicula;
 
 
